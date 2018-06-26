96665452db858c26184d3c3660e7f70b3cf11e65
Ross Qualey <qualeyro@gmail.com>
2018-06-26 00:53:56 -0500

    completed after studying Devs live solve video

--- client.js
+++ client.js
@@ -1,20 +1,34 @@
-console.log('JS');
-
 $(document).ready(readyNow);
 
-function readyNow() {
-    console.log('JQ');
-    $('#btnGenerate').on('click', handleClick);
+function readyNow(){
+    $('#btnGenerate').on('click', generateDiv);
+    $(document).on('click', '.deleteBtn', deleteDiv);
+    $(document).on('click', '.swapBtn', swapColors);
 }
 
-function handleClick() {
-    console.log('click!');
+let numClicks = 0;
+
+function generateDiv() {
+    numClicks++;
+    // console.log('In generateDiv');
+    // console.log('In Delete');
+    // console.log('In Swap');
+    let div = $('#main');
+    let output = '<div class="red">';
+        output += '<p>' + numClicks + '</p>';
+        output += '<button class="swapBtn">Swap</button>';
+        output += '<button class="deleteBtn">Delete</button>';
+    output += '</div>';
+    div.append(output);
 }
 
-function addEventHandlers() {
-    $('#btnGenerate').on('click', redDiv);
+function deleteDiv() {
+    // console.log('In deleteDiv', $(this).parent());
+    $(this).parent().remove();
+    
 }
 
-function redDiv() {
-    $('#colorSwap').append('<div class="#color One"></div>');
+function swapColors() {
+    // console.log('In swapColors', $(this).parent());
+    $(this).parent().toggleClass('yellow');
 }
